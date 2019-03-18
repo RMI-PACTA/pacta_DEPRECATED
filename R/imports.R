504b3c6c4bc8c16ec034e5645ffccc448bb2c94b
EvgenyPetrovsky <fonpetroff@mail.ru>
2019-03-18 23:08:16 +0100

    remove pipe useless description

--- R/imports.R
+++ R/imports.R
@@ -1,14 +1,4 @@
-#' Pipe functions
-#'
-#' Like dplyr, PACTA uses the pipe function, \code{\%>\%} to turn
-#' function composition into a series of imperative statements.
-#'
 #' @importFrom magrittr %>%
-#' @name %>%
-#' @rdname pipe
-#' @param lhs,rhs An R object and a function to apply to it
-NULL
-
 #' @importFrom dplyr if_else case_when
 #' @importFrom tibble as_tibble
 NULL
