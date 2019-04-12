a2c7feb743bfe81e07243a95167553ff40b70883
Evgeny Petrovsky <evgeny.petrovsky@ing.nl>
2019-04-12 15:09:32 +0200

    generate desired number of rows and use seed as a parameterized value

--- data-raw/test-loanbook.R
+++ data-raw/test-loanbook.R
@@ -4,32 +4,35 @@ data.file <- utils::read.csv(
   colClasses = "character", header = T, stringsAsFactors = F)
 
 #generate columns
-rows <- nrow(data.file)
+seed <- 1001
+rows <- 1000
+
+data <- data.file[sample(x = nrow(data.file), size = rows, replace = T),]
 
 data.test.loanbook <- data.frame(
   Source = "pacta::data.test.loanbook",
   Reporting.Date = Sys.Date(),
   Facility.ID = paste("Facility", substr(0.1E10 + 1:rows, 3, 10), sep = "-"),
-  Outstanding = {set.seed(1001); rpois(rows, 1e3) * 1e3},
-  Exposure    = {set.seed(1001); rpois(rows, 1e3) * 1e3},
+  Outstanding = {set.seed(seed); rpois(rows, 1e3) * 1e3},
+  Exposure    = {set.seed(seed); rpois(rows, 1e3) * 1e3},
   Loan.Currency = "USD",
-  Maturity.Date = Sys.Date() + as.integer({set.seed(2001); rpois(rows, 52)} * 7 * 3),
+  Maturity.Date = Sys.Date() + as.integer({set.seed(seed+1000); rpois(rows, 52)} * 7 * 3),
 
   Asset.ID = "",
   Asset.Name = "",
-  Borrower.ID = data.file$Company.Name,
-  Borrower.Name = data.file$Company.Name,
-  Borrower.Country = data.file$Country,
-  Borrower.NAICS = data.file$NAICS,
-  Borrower.NAICS.Description = data.file$NAICS.Description,
+  Borrower.ID = data$Company.Name,
+  Borrower.Name = data$Company.Name,
+  Borrower.Country = data$Country,
+  Borrower.NAICS = data$NAICS,
+  Borrower.NAICS.Description = data$NAICS.Description,
 
-  Sector.Classification = pacta::convert.naics2sc(data.file$NAICS),
+  Sector.Classification = pacta::convert.naics2sc(data$NAICS),
 
-  Ultimate.Parent.ID = data.file$Company.Name,
-  Ultimate.Parent.Name = data.file$Company.Name,
-  Ultimate.Parent.Country = data.file$Country,
-  Ultimate.Parent.NAICS = data.file$NAICS,
-  Ultimate.Parent.NAICS.Description = data.file$NAICS.Description
+  Ultimate.Parent.ID = data$Company.Name,
+  Ultimate.Parent.Name = data$Company.Name,
+  Ultimate.Parent.Country = data$Country,
+  Ultimate.Parent.NAICS = data$NAICS,
+  Ultimate.Parent.NAICS.Description = data$NAICS.Description
 )
 #save
 devtools::use_data(data.test.loanbook, overwrite = T)
