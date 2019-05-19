5e5e6a6171911b29f70ebd7243c20309fd257c57
Mauro Lepore <maurolepore@gmail.com>
2019-05-19 07:57:36 -0400

    Flag `subset()` used in a function.

--- R/core-datacheck-functions.R
+++ R/core-datacheck-functions.R
@@ -30,6 +30,12 @@ check.ds_cols <- function(ds_name, ds_col_names) {
 #' @param ds_name Dataset Name
 #' @param ds_col_names vector of Dataset columns
 get.ds_missing_cols <- function(ds_name, ds_col_names) {
+  # FIXME: See `?subset()`
+  # Warning
+  # This is a convenience function intended for use interactively. For
+  # programming it is better to use the standard subsetting functions like [,
+  # and in particular the non-standard evaluation of argument subset can have
+  # unanticipated consequences.
   requirements <- subset(
     # FIXME: 
     # `pacta::dataset.req.attrs` does not exist.
